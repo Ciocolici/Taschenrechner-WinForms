31ce3fff16f9cbfffdb4a7af038c50b1a19839fd
Ciocolici <68241693+Ciocolici@users.noreply.github.com>
2023-09-27 15:59:41 +0200

    updated the project

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Taschenrechner_V2
 {
-    partial class Form1
+    partial class Taschenrechner
     {
         /// <summary>
         ///  Required designer variable.
@@ -46,7 +46,6 @@
             buttonFour = new Button();
             fieldOutput = new TextBox();
             fieldInput = new RichTextBox();
-            buttonSpecial = new Button();
             buttonCE = new Button();
             buttonC = new Button();
             SuspendLayout();
@@ -57,14 +56,14 @@
             buttonPlus.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
             buttonPlus.ForeColor = Color.Cornsilk;
             buttonPlus.ImageAlign = ContentAlignment.MiddleRight;
-            buttonPlus.Location = new Point(263, 458);
+            buttonPlus.Location = new Point(249, 426);
             buttonPlus.Name = "buttonPlus";
             buttonPlus.Size = new Size(73, 83);
             buttonPlus.TabIndex = 2;
             buttonPlus.Text = "+";
             buttonPlus.TextAlign = ContentAlignment.TopCenter;
             buttonPlus.UseVisualStyleBackColor = false;
-            buttonPlus.Click += button1_Click_1;
+            buttonPlus.Click += buttonPlus_Click;
             // 
             // buttonMinus
             // 
@@ -72,13 +71,14 @@
             buttonMinus.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
             buttonMinus.ForeColor = Color.Cornsilk;
             buttonMinus.ImageAlign = ContentAlignment.MiddleRight;
-            buttonMinus.Location = new Point(263, 369);
+            buttonMinus.Location = new Point(249, 337);
             buttonMinus.Name = "buttonMinus";
             buttonMinus.Size = new Size(73, 83);
             buttonMinus.TabIndex = 3;
             buttonMinus.Text = "-";
             buttonMinus.TextAlign = ContentAlignment.TopCenter;
             buttonMinus.UseVisualStyleBackColor = false;
+            buttonMinus.Click += buttonMinus_Click;
             // 
             // buttonMultiply
             // 
@@ -86,13 +86,14 @@
             buttonMultiply.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
             buttonMultiply.ForeColor = Color.Cornsilk;
             buttonMultiply.ImageAlign = ContentAlignment.MiddleRight;
-            buttonMultiply.Location = new Point(263, 276);
+            buttonMultiply.Location = new Point(249, 248);
             buttonMultiply.Name = "buttonMultiply";
             buttonMultiply.Size = new Size(73, 83);
             buttonMultiply.TabIndex = 4;
             buttonMultiply.Text = "x";
             buttonMultiply.TextAlign = ContentAlignment.TopCenter;
             buttonMultiply.UseVisualStyleBackColor = false;
+            buttonMultiply.Click += buttonMultiply_Click;
             // 
             // buttonDivide
             // 
@@ -100,14 +101,14 @@
             buttonDivide.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
             buttonDivide.ForeColor = Color.Cornsilk;
             buttonDivide.ImageAlign = ContentAlignment.MiddleRight;
-            buttonDivide.Location = new Point(263, 187);
+            buttonDivide.Location = new Point(249, 159);
             buttonDivide.Name = "buttonDivide";
             buttonDivide.Size = new Size(73, 83);
             buttonDivide.TabIndex = 5;
             buttonDivide.Text = ":";
             buttonDivide.TextAlign = ContentAlignment.TopCenter;
             buttonDivide.UseVisualStyleBackColor = false;
-            buttonDivide.Click += button5_Click;
+            buttonDivide.Click += buttonDivide_Click;
             // 
             // buttonEqual
             // 
@@ -115,14 +116,14 @@
             buttonEqual.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
             buttonEqual.ForeColor = Color.Cornsilk;
             buttonEqual.ImageAlign = ContentAlignment.MiddleRight;
-            buttonEqual.Location = new Point(170, 458);
+            buttonEqual.Location = new Point(170, 426);
             buttonEqual.Name = "buttonEqual";
             buttonEqual.Size = new Size(73, 83);
             buttonEqual.TabIndex = 6;
             buttonEqual.Text = "=";
             buttonEqual.TextAlign = ContentAlignment.TopCenter;
             buttonEqual.UseVisualStyleBackColor = false;
-            buttonEqual.Click += button6_Click;
+            buttonEqual.Click += buttonEqual_Click;
             // 
             // buttonComma
             // 
@@ -130,13 +131,14 @@
             buttonComma.Font = new Font("Segoe UI", 36F, FontStyle.Regular, GraphicsUnit.Point);
             buttonComma.ForeColor = Color.Black;
             buttonComma.ImageAlign = ContentAlignment.MiddleRight;
-            buttonComma.Location = new Point(12, 458);
+            buttonComma.Location = new Point(12, 426);
             buttonComma.Name = "buttonComma";
             buttonComma.Size = new Size(73, 83);
             buttonComma.TabIndex = 7;
-            buttonComma.Text = ".";
+            buttonComma.Text = ",";
             buttonComma.TextAlign = ContentAlignment.TopCenter;
             buttonComma.UseVisualStyleBackColor = false;
+            buttonComma.Click += buttonComma_Click;
             // 
             // buttonZero
             // 
@@ -144,13 +146,14 @@
             buttonZero.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonZero.ForeColor = Color.Black;
             buttonZero.ImageAlign = ContentAlignment.MiddleRight;
-            buttonZero.Location = new Point(91, 458);
+            buttonZero.Location = new Point(91, 426);
             buttonZero.Name = "buttonZero";
             buttonZero.Size = new Size(73, 83);
             buttonZero.TabIndex = 8;
             buttonZero.Text = "0";
             buttonZero.TextAlign = ContentAlignment.TopCenter;
             buttonZero.UseVisualStyleBackColor = false;
+            buttonZero.Click += buttonNumber_Click;
             // 
             // buttonThree
             // 
@@ -158,13 +161,14 @@
             buttonThree.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonThree.ForeColor = Color.Black;
             buttonThree.ImageAlign = ContentAlignment.MiddleRight;
-            buttonThree.Location = new Point(170, 369);
+            buttonThree.Location = new Point(170, 337);
             buttonThree.Name = "buttonThree";
             buttonThree.Size = new Size(73, 83);
             buttonThree.TabIndex = 18;
             buttonThree.Text = "3";
             buttonThree.TextAlign = ContentAlignment.TopCenter;
             buttonThree.UseVisualStyleBackColor = false;
+            buttonThree.Click += buttonNumber_Click;
             // 
             // buttonTwo
             // 
@@ -172,13 +176,14 @@
             buttonTwo.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonTwo.ForeColor = Color.Black;
             buttonTwo.ImageAlign = ContentAlignment.MiddleRight;
-            buttonTwo.Location = new Point(91, 369);
+            buttonTwo.Location = new Point(91, 337);
             buttonTwo.Name = "buttonTwo";
             buttonTwo.Size = new Size(73, 83);
             buttonTwo.TabIndex = 19;
             buttonTwo.Text = "2";
             buttonTwo.TextAlign = ContentAlignment.TopCenter;
             buttonTwo.UseVisualStyleBackColor = false;
+            buttonTwo.Click += buttonNumber_Click;
             // 
             // buttonNine
             // 
@@ -186,13 +191,14 @@
             buttonNine.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonNine.ForeColor = Color.Black;
             buttonNine.ImageAlign = ContentAlignment.MiddleRight;
-            buttonNine.Location = new Point(170, 187);
+            buttonNine.Location = new Point(170, 159);
             buttonNine.Name = "buttonNine";
             buttonNine.Size = new Size(73, 83);
             buttonNine.TabIndex = 20;
             buttonNine.Text = "9";
             buttonNine.TextAlign = ContentAlignment.TopCenter;
             buttonNine.UseVisualStyleBackColor = false;
+            buttonNine.Click += buttonNumber_Click;
             // 
             // buttonEight
             // 
@@ -200,13 +206,14 @@
             buttonEight.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonEight.ForeColor = Color.Black;
             buttonEight.ImageAlign = ContentAlignment.MiddleRight;
-            buttonEight.Location = new Point(91, 187);
+            buttonEight.Location = new Point(91, 159);
             buttonEight.Name = "buttonEight";
             buttonEight.Size = new Size(73, 83);
             buttonEight.TabIndex = 21;
             buttonEight.Text = "8";
             buttonEight.TextAlign = ContentAlignment.TopCenter;
             buttonEight.UseVisualStyleBackColor = false;
+            buttonEight.Click += buttonNumber_Click;
             // 
             // buttonFive
             // 
@@ -214,13 +221,14 @@
             buttonFive.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonFive.ForeColor = Color.Black;
             buttonFive.ImageAlign = ContentAlignment.MiddleRight;
-            buttonFive.Location = new Point(91, 276);
+            buttonFive.Location = new Point(91, 248);
             buttonFive.Name = "buttonFive";
             buttonFive.Size = new Size(73, 83);
             buttonFive.TabIndex = 22;
             buttonFive.Text = "5";
             buttonFive.TextAlign = ContentAlignment.TopCenter;
             buttonFive.UseVisualStyleBackColor = false;
+            buttonFive.Click += buttonNumber_Click;
             // 
             // buttonOne
             // 
@@ -228,13 +236,14 @@
             buttonOne.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonOne.ForeColor = Color.Black;
             buttonOne.ImageAlign = ContentAlignment.MiddleRight;
-            buttonOne.Location = new Point(12, 369);
+            buttonOne.Location = new Point(12, 337);
             buttonOne.Name = "buttonOne";
             buttonOne.Size = new Size(73, 83);
             buttonOne.TabIndex = 23;
             buttonOne.Text = "1";
             buttonOne.TextAlign = ContentAlignment.TopCenter;
             buttonOne.UseVisualStyleBackColor = false;
+            buttonOne.Click += buttonNumber_Click;
             // 
             // buttonSix
             // 
@@ -242,13 +251,14 @@
             buttonSix.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonSix.ForeColor = Color.Black;
             buttonSix.ImageAlign = ContentAlignment.MiddleRight;
-            buttonSix.Location = new Point(170, 276);
+            buttonSix.Location = new Point(170, 248);
             buttonSix.Name = "buttonSix";
             buttonSix.Size = new Size(73, 83);
             buttonSix.TabIndex = 24;
             buttonSix.Text = "6";
             buttonSix.TextAlign = ContentAlignment.TopCenter;
             buttonSix.UseVisualStyleBackColor = false;
+            buttonSix.Click += buttonNumber_Click;
             // 
             // buttonSeven
             // 
@@ -256,13 +266,14 @@
             buttonSeven.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonSeven.ForeColor = Color.Black;
             buttonSeven.ImageAlign = ContentAlignment.MiddleRight;
-            buttonSeven.Location = new Point(12, 187);
+            buttonSeven.Location = new Point(12, 159);
             buttonSeven.Name = "buttonSeven";
             buttonSeven.Size = new Size(73, 83);
             buttonSeven.TabIndex = 25;
             buttonSeven.Text = "7";
             buttonSeven.TextAlign = ContentAlignment.TopCenter;
             buttonSeven.UseVisualStyleBackColor = false;
+            buttonSeven.Click += buttonNumber_Click;
             // 
             // buttonFour
             // 
@@ -270,86 +281,68 @@
             buttonFour.Font = new Font("Yu Gothic Medium", 36F, FontStyle.Bold, GraphicsUnit.Point);
             buttonFour.ForeColor = Color.Black;
             buttonFour.ImageAlign = ContentAlignment.MiddleRight;
-            buttonFour.Location = new Point(12, 276);
+            buttonFour.Location = new Point(12, 248);
             buttonFour.Name = "buttonFour";
             buttonFour.Size = new Size(73, 83);
             buttonFour.TabIndex = 26;
             buttonFour.Text = "4";
             buttonFour.TextAlign = ContentAlignment.TopCenter;
             buttonFour.UseVisualStyleBackColor = false;
-            buttonFour.Click += button15_Click;
+            buttonFour.Click += buttonNumber_Click;
             // 
             // fieldOutput
             // 
+            fieldOutput.Enabled = false;
             fieldOutput.Location = new Point(12, 12);
             fieldOutput.Name = "fieldOutput";
             fieldOutput.ReadOnly = true;
-            fieldOutput.Size = new Size(324, 23);
+            fieldOutput.Size = new Size(310, 23);
             fieldOutput.TabIndex = 27;
-            fieldOutput.TextChanged += textBox1_TextChanged;
+            fieldOutput.TextChanged += fieldOutput_TextChanged;
             // 
             // fieldInput
             // 
+            fieldInput.Enabled = false;
             fieldInput.Location = new Point(12, 41);
             fieldInput.Name = "fieldInput";
             fieldInput.ReadOnly = true;
-            fieldInput.Size = new Size(324, 51);
+            fieldInput.Size = new Size(310, 51);
             fieldInput.TabIndex = 28;
             fieldInput.Text = "";
-            // 
-            // buttonSpecial
-            // 
-            buttonSpecial.BackColor = SystemColors.ControlLight;
-            buttonSpecial.Font = new Font("Yu Gothic Medium", 25F, FontStyle.Bold, GraphicsUnit.Point);
-            buttonSpecial.ForeColor = Color.Black;
-            buttonSpecial.ImageAlign = ContentAlignment.MiddleRight;
-            buttonSpecial.Location = new Point(12, 132);
-            buttonSpecial.Name = "buttonSpecial";
-            buttonSpecial.Size = new Size(73, 49);
-            buttonSpecial.TabIndex = 29;
-            buttonSpecial.Text = "%";
-            buttonSpecial.TextAlign = ContentAlignment.TopCenter;
-            buttonSpecial.UseVisualStyleBackColor = false;
+            fieldInput.TextChanged += fieldInput_TextChanged;
             // 
             // buttonCE
             // 
-            buttonCE.BackColor = SystemColors.ControlLight;
-            buttonCE.Font = new Font("Yu Gothic Medium", 25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonCE.Font = new Font("Yu Gothic Medium", 8.25F, FontStyle.Bold, GraphicsUnit.Point);
             buttonCE.ForeColor = Color.Black;
-            buttonCE.ImageAlign = ContentAlignment.MiddleRight;
-            buttonCE.Location = new Point(91, 132);
+            buttonCE.Location = new Point(12, 113);
             buttonCE.Name = "buttonCE";
-            buttonCE.Size = new Size(73, 49);
-            buttonCE.TabIndex = 30;
+            buttonCE.Size = new Size(152, 40);
+            buttonCE.TabIndex = 29;
             buttonCE.Text = "CE";
-            buttonCE.TextAlign = ContentAlignment.TopCenter;
-            buttonCE.UseVisualStyleBackColor = false;
-            buttonCE.Click += button18_Click;
+            buttonCE.UseVisualStyleBackColor = true;
+            buttonCE.Click += buttonCE_Click;
             // 
             // buttonC
             // 
-            buttonC.BackColor = SystemColors.ControlLight;
-            buttonC.Font = new Font("Yu Gothic Medium", 25F, FontStyle.Bold, GraphicsUnit.Point);
+            buttonC.Font = new Font("Yu Gothic Medium", 9F, FontStyle.Bold, GraphicsUnit.Point);
             buttonC.ForeColor = Color.Black;
-            buttonC.ImageAlign = ContentAlignment.MiddleRight;
-            buttonC.Location = new Point(170, 132);
+            buttonC.Location = new Point(170, 113);
             buttonC.Name = "buttonC";
-            buttonC.Size = new Size(73, 49);
-            buttonC.TabIndex = 31;
+            buttonC.Size = new Size(152, 40);
+            buttonC.TabIndex = 30;
             buttonC.Text = "C";
-            buttonC.TextAlign = ContentAlignment.TopCenter;
-            buttonC.UseVisualStyleBackColor = false;
-            buttonC.Click += button19_Click;
+            buttonC.UseVisualStyleBackColor = true;
+            buttonC.Click += buttonC_Click;
             // 
-            // Form1
+            // Taschenrechner
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ControlDark;
-            ClientSize = new Size(351, 553);
+            ClientSize = new Size(337, 521);
             Controls.Add(buttonC);
             Controls.Add(buttonCE);
-            Controls.Add(buttonSpecial);
             Controls.Add(fieldInput);
             Controls.Add(fieldOutput);
             Controls.Add(buttonFour);
@@ -368,8 +361,8 @@
             Controls.Add(buttonMultiply);
             Controls.Add(buttonMinus);
             Controls.Add(buttonPlus);
-            Name = "Form1";
-            Text = "Form1";
+            Name = "Taschenrechner";
+            Text = "Taschenrechner";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -383,7 +376,6 @@
         private Button buttonComma;
         private Button buttonZero;
         private Button buttonThree;
-        private Button buttonTwo;
         private Button buttonNine;
         private Button buttonEight;
         private Button buttonFive;
@@ -393,7 +385,7 @@
         private Button buttonFour;
         private TextBox fieldOutput;
         private RichTextBox fieldInput;
-        private Button buttonSpecial;
+        private Button buttonTwo;
         private Button buttonCE;
         private Button buttonC;
     }
--- Form1.cs
+++ Form1.cs
@@ -1,59 +1,122 @@
 namespace Taschenrechner_V2
 {
-    public partial class Form1 : Form
+    public partial class Taschenrechner : Form
     {
-        public Form1()
+        double number1;
+        double number2;
+        string operation;
+        double sum;
+        public Taschenrechner()
         {
             InitializeComponent();
-
-            fieldOutput.Text = "";
-            fieldInput.Text = "";
         }
-
-        private void button1_Click(object sender, EventArgs e)
+        private void buttonNumber_Click(object sender, EventArgs e)
         {
-            Button btn = (Button)sender;
-            fieldInput.Text += btn.Text.ToString() + " ";
+            Button buttonNumber = sender as Button;
+            if (buttonNumber != null)
+            {
+                fieldInput.Text += buttonNumber.Text;
+            }
         }
 
-        private void button1_Click_1(object sender, EventArgs e)
+        private void buttonEqual_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(fieldInput.Text))
+            {
+                number2 = double.Parse(fieldInput.Text);
+
+                switch (operation)
+                {
+                    case "+":
+                        sum = number1 + number2;
+                        break;
+                    case "-":
+                        sum = number1 - number2;
+                        break;
+                    case "*":
+                        sum = number1 * number2;
+                        break;
+                    case ":":
+                        sum = number1 / number2;
+                        break;
+                }
+
+                fieldOutput.Text = sum.ToString();
+                fieldInput.Text = "";
+            }
+        }
+
+        private void buttonPlus_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(fieldInput.Text))
+            {
+                number1 = double.Parse(fieldInput.Text);
+                operation = "+";
+                fieldInput.Clear();
+            }
+        }
+
+        private void buttonComma_Click(object sender, EventArgs e)
+        {
+            if (!fieldInput.Text.Contains("."))
+            {
+                fieldInput.Text += ".";
+            }
+        }
+
+        private void buttonMinus_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(fieldInput.Text))
+            {
+                number1 = double.Parse(fieldInput.Text);
+                operation = "-";
+                fieldInput.Clear();
+            }
+        }
+
+        private void buttonMultiply_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(fieldInput.Text))
+            {
+                number1 = double.Parse(fieldInput.Text);
+                operation = "*";
+                fieldInput.Clear();
+            }
+        }
+
+        private void buttonDivide_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(fieldInput.Text))
+            {
+                number1 = double.Parse(fieldInput.Text);
+                operation = ":";
+                fieldInput.Clear();
+            }
+        }
+
+        private void fieldInput_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void fieldOutput_TextChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void buttonCE_Click(object sender, EventArgs e)
         {
-
+            fieldInput.Clear();
         }
 
-        private void button15_Click(object sender, EventArgs e)
+        private void buttonC_Click(object sender, EventArgs e)
         {
-
+            fieldInput.Clear();
+            fieldOutput.Clear();
+            number1 = 0;
+            number2 = 0;
+            operation = "";
         }
 
-        private void button5_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button19_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button18_Click(object sender, EventArgs e)
-        {
-
-        }
     }
 }
